a3eed0338161c4b01d2a3373500d457ea8f38580
hwy113329@163.com <hwy113329@163.com>
2022-05-28 18:56:31 +0800

    添加GitHub desktop

--- src/main/java/com/example/gitdemo/OnlyTest.java
+++ src/main/java/com/example/gitdemo/OnlyTest.java
@@ -4,5 +4,6 @@ public class OnlyTest {
     public static void main(String[] args) {
         System.out.println("另外一个分支 回退前");
         System.out.println("添加GitHub desktop");
+        System.out.println("====提交");
     }
 }
